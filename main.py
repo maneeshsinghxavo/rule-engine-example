5535e1765f1e65e1b6b137c0a9789371b9cdc2c9
s1manesi <maneesh.singh@dxc.com>
2023-09-02 22:17:36 +0200

    added rule engine  and  json data parsing

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
+import json
+import yaml
+import rule_engine
+import re
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+re_flags = re.IGNORECASE | re.MULTILINE
+context = rule_engine.Context(default_value=None, regex_flags=re_flags)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def log_violations():
+    f = open('compiler_data.json')
+    results = json.load(f)
+
+    with open('rules.yml', 'r') as file_h:
+        rules = yaml.load(file_h, Loader=yaml.FullLoader)
+
+    for violation in rules['rules']:
+        try:
+            rule = rule_engine.Rule(violation['rule'], context=context)
+        except rule_engine.RuleSyntaxError as error:
+            print(error.message)
+
+        matches = rule.filter(results)
+        if not matches:
+            continue
+
+        for match in matches:
+            target_name = match['name']
+            print('{} {} violation'.format(target_name, violation['description']))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    log_violations()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
